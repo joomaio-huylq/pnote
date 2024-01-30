9034b224c42accd73a29736e960d38e28f346a6d
joomaio.huylq <dauxotcachua99@gmail.com>
2024-01-30 16:39:21 +0700

    test button

---
+++ pnote/registers/AfterInstall.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\pnote\pnote\registers;
+
+use SPT\Application\IApp;
+use SPT\Support\Loader;
+
+class Menu
+{
+    public static function registerButton( IApp $app )
+    {
+        return '123';
+    }
+}
